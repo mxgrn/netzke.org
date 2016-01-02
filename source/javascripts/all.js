21b3fd7e3d65a8f87176dbd6800a5192e9a44b69
Max Gorin <max@goodbitlabs.com>
2016-01-02 21:41:37 +0700

    Revert "Embed intro video"
    This reverts commit b25fe1c4d86279846f15e9f47cc66297b6951941.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,3 @@
 //= require jquery  
 //= require bootstrap-sprockets
 //= require_tree .
-
-$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox({always_show_close: true, width: 1000});
-});
